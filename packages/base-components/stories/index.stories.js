889b2f455f8504fd73fd7e06b8042774cb41066b
Jenna Vuong <jvuong@alum.berkeley.edu>
2019-05-14 18:17:07 -0700

    configure storybook to run stories from separate packages inside 'package' folder

--- packages/base-components/stories/index.stories.js
+++ packages/base-components/stories/index.stories.js
@@ -5,7 +5,7 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
-import BaseButton from '../packages/base-components/src/BaseButton';
+import BaseButton from '../src/BaseButton';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
